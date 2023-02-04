896cfe1e52c277bf1e1ab68b15dd2b751ef72c73
pborisenko <pborisenko@openwaygroup.com>
2023-02-04 15:57:31 +0800

    custom parsing

--- src/parsers/CustomParser.ts
+++ src/parsers/CustomParser.ts
@@ -3,6 +3,12 @@ import IParser from "./IParser";
 
 
 export default class CustomParser implements IParser {
+    fileType: String
+    
+    constructor(fileType: String) {
+        this.fileType = fileType
+    }
+
     async parse(file: File): Promise<MetaLine[]> {
         return [] as MetaLine[]
     }
